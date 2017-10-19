6f4392993ca48037513e53f81327fc58e7d0bb08
omri cohen <omrichoen@gmail.com>
2017-10-19 21:23:02 +0300

    added drivetrain

--- Spikes-Lib-Testing/src/org/usfirst/frc/team2212/robot/Robot.java
+++ Spikes-Lib-Testing/src/org/usfirst/frc/team2212/robot/Robot.java
@@ -1,6 +1,12 @@
 
 package org.usfirst.frc.team2212.robot;
 
+import com.ctre.CANTalon;
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
+import com.spikes2212.utils.DoubleSpeedcontroller;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -21,6 +27,7 @@ public class Robot extends IterativeRobot {
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
+	TankDrivetrain drivetrain;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -28,7 +35,13 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
+		drivetrain = new TankDrivetrain(
+				new DoubleSpeedcontroller(new CANTalon(RobotMap.CAN.DRIVE_LEFT_1),
+						new CANTalon(RobotMap.CAN.DRIVE_LEFT_2))::set,
+				new DoubleSpeedcontroller(new CANTalon(RobotMap.CAN.DRIVE_LEFT_1),
+						new CANTalon(RobotMap.CAN.DRIVE_LEFT_2))::set);
 		oi = new OI();
+		drivetrain.setDefaultCommand(new DriveArcade(drivetrain, oi::getForward, oi::getRotation));
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 	}
--- Spikes-Lib-Testing/src/org/usfirst/frc/team2212/robot/RobotMap.java
+++ Spikes-Lib-Testing/src/org/usfirst/frc/team2212/robot/RobotMap.java
@@ -7,13 +7,10 @@ package org.usfirst.frc.team2212.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	public interface CAN {
+		public static final int DRIVE_LEFT_1 = 6;
+		public static final int DRIVE_LEFT_2 = 3;
+		public static final int DRIVE_RIGHT_1 = 5;
+		public static final int DRIVE_RIGHT_2 = 1;
+	}
 }
