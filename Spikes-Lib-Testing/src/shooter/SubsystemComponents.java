7c6850ba03b4b77100b94eb174af0e9903db97a1
Matangn13 <matangn13@gmail.com>
2018-05-31 20:48:36 +0300

    Added PID Settings

---
+++ Spikes-Lib-Testing/src/shooter/SubsystemComponents.java
@@ -0,0 +1,20 @@
+package shooter;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.utils.DoubleSpeedcontroller;
+import com.spikes2212.utils.PIDSettings;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.VictorSP;
+
+public class SubsystemComponents {
+
+	public static class  Shooter {
+		public static final VictorSP MOTOR = new VictorSP(RobotMap.PWM.ROLLER_MOTOR);
+		public static final Encoder ENCODER = new Encoder(RobotMap.DIO.ENCODER_A, RobotMap.DIO.ENCODER_B);
+		
+		public static final PIDSettings PID_SETTINGS = new PIDSettings(SubsystemConstants.Shooter.KP.get(), SubsystemConstants.Shooter.KI.get(), SubsystemConstants.Shooter.KD.get(), 0, 1);
+		
+	}
+
+}
