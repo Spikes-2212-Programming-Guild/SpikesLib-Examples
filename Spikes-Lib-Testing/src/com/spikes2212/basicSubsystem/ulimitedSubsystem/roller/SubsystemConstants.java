a464fcac105159afeece33a1fa07610553662c29
Matangn13 <matangn13@gmail.com>
2018-05-31 19:43:42 +0300

    Added rollOut speed

--- Spikes-Lib-Testing/src/com/spikes2212/basicSubsystem/ulimitedSubsystem/roller/SubsystemConstants.java
+++ Spikes-Lib-Testing/src/com/spikes2212/basicSubsystem/ulimitedSubsystem/roller/SubsystemConstants.java
@@ -6,6 +6,7 @@ import com.spikes2212.dashboard.ConstantHandler;
 
 public class SubsystemConstants {
 	public static class roller {
-		public static final Supplier<Double> ROLLER_IN_SPEED = ConstantHandler.addConstantDouble("Roller In Speed", 2212);
+		public static final Supplier<Double> ROLLER_IN_SPEED = ConstantHandler.addConstantDouble("Roller In Speed", 0.3);
+		public static final Supplier<Double> ROLLER_OUT_SPEED = ConstantHandler.addConstantDouble("Roller Out Speed", -0.3);
 	}
 }
