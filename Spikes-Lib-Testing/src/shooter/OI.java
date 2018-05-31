8c22892ee0601a3f50dbf2ec8330c16427578af7
Matangn13 <matangn13@gmail.com>
2018-05-31 20:51:30 +0300

    Added a Button to shoot, and it's command

---
+++ Spikes-Lib-Testing/src/shooter/OI.java
@@ -0,0 +1,24 @@
+package shooter;
+
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemWithPIDForSpeed;
+import com.spikes2212.utils.PIDSettings;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	private Joystick joystick = new Joystick(0);
+
+	public OI() {
+		JoystickButton shoot = new JoystickButton(joystick, 2);
+
+		shoot.whenPressed(new MoveBasicSubsystemWithPIDForSpeed(Robot.shooter, SubsystemComponents.Shooter.ENCODER, SubsystemConstants.Shooter.SHOOT_SPEED, SubsystemComponents.Shooter.PID_SETTINGS, SubsystemConstants.Shooter.SHOOT_ACCELERATION.get()));
+		
+	}
+
+}
