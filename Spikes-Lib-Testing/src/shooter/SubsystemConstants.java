45a682165f10bb5ac78ad7af85a4ae80cea9fd13
omri00 <omrichoen@gmail.com>
2018-06-07 20:13:52 +0300

    Update SubsystemConstants.java
    made constant make sence

--- Spikes-Lib-Testing/src/shooter/SubsystemConstants.java
+++ Spikes-Lib-Testing/src/shooter/SubsystemConstants.java
@@ -7,7 +7,7 @@ import com.spikes2212.dashboard.ConstantHandler;
 public class SubsystemConstants {
 	public static class Shooter {
 		public static final Supplier<Double> SHOOT_SPEED = ConstantHandler.addConstantDouble("Shoot Speed", 1000);
-		public static final Supplier<Double> SHOOT_ACCELERATION = ConstantHandler.addConstantDouble("Shoot Acceleration", 1000);
+		public static final Supplier<Double> SHOOT_ACCELERATION = ConstantHandler.addConstantDouble("Shoot Acceleration", 0.1);
 		
 		public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("Shooter PID's KP", 07);
 		public static final Supplier<Double> KI= ConstantHandler.addConstantDouble("Shooter PID's KI", 01);
